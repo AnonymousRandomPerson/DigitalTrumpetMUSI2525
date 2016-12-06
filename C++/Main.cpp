27eb2db1cf631a39ba5f75273a34e8ab932057d0
Schrum, Coulter S <cschrum3@gatech.edu>
2016-12-06 17:14:47 -0500

    Update and rename Dummy.cpp to Main.cpp

--- C++/Main.cpp
+++ C++/Main.cpp
@@ -2,15 +2,35 @@
 #include "PinDetect.h"
 #include "Speaker.h"
 
+
 // setup builtin LEDs
 DigitalOut myLed1(LED1);
 DigitalOut myLed2(LED2);
 DigitalOut myLed3(LED3);
 DigitalOut myLed4(LED4);
 
+//Hardware Setup
+
+// setup vibration sensor
+  AnalogIn vibration(p17);
+  float vreading;
+//setup pressure sensor
+  AnalogIn pressure(p18);
+  float preading;
+  
+  
+// Patterns for choosing which valve switch statement to use.
+  enum Pattern { pattern_a, pattern_b };
+// The current on/off status of the valves 123.
+  enum ValveStatus { v000, v100, v010, v110, v001, v101, v011, v111 };
+// setup pc serial monitor
+  Serial pc(USBTX, USBRX);
+ 
+
 // setup pushbuttons for debounced interrupt use
 PinDetect pb1(p23);
 PinDetect pb2(p24);
+PinDetect pb3(p25);
 
 // setup instance of new Speaker class, mySpeaker using pin 21
 // the pin must be a PWM output pin
@@ -18,6 +38,7 @@ Speaker mySpeaker(p21);
 
 // pushbutton status - need volatile for safe interrupt R/W
 volatile int pbStatus = 0;
+volatile int range = 0;
 
 // Callback routine is interrupt activated by a debounced pb1 hit
 void pb1_hit_callback (void)
@@ -32,39 +53,71 @@ void pb2_hit_callback (void)
     // CODE HERE WILL RUN WHEN INTERUPT IS GENERATED
     pbStatus = pbStatus | 0x02;
 }
+void pb3_hit_callback (void)
+{
+    // CODE HERE WILL RUN WHEN INTERUPT IS GENERATED
+    pbStatus = pbStatus | 0x04;
 
+}
 // main program
-int main()
+void main()
 {
 //setup four SPST push buttons
     pb1.mode(PullUp); //add internal pullup resistor
     pb2.mode(PullUp);
+    pb3.mode(PullUp);
     // need a small delay for initial pullup to take effect due to capacitance
     wait(.01);
     // Setup Interrupt callback functions for each pb hit
     pb1.attach_deasserted(&pb1_hit_callback);
     pb2.attach_deasserted(&pb2_hit_callback);
+    pb3.attach_deasserted(&pb3_hit_callback);
     // Start sampling the pb inputs using interrupts
     pb1.setSampleFrequency();
     pb2.setSampleFrequency();
-// pushbuttons are all now setup with pullups and running with a software decounce filter
+    pb3.setSampleFrequency();
+// pushbuttons are setup with pullups and running with a software debounce filter
+
+//boolean valves
+    bool valve1 = 0;
+    bool valve2 = 0;
+    bool valve3 = 0;
 
 //main loop 
     while(1) {
-        //check pushbutton status for new input
+    //read and set vibration range
+            vreading = vibration.read();
+            pc.printf(" vibration = [%f]\n\r", vreading); 
+    //read and set pressure range
+            preading = pressure.read();
+            pc.printf(" pressure = [%f]\n\r\n", preading);
+       //check pushbutton status for new input
+       
+
+       
         switch (pbStatus) {
-                //move forward
             case 0x01 :
                 myLed2 = 1;
-                pbStatus = 0;
+                wait(.01);
+                myLed2 = 0;
                 mySpeaker.PlayNote(200.0,0.1,0.8);
                 break;
-                //move backwards
             case 0x02 :
+                //blink light for debugging
                 myLed3 = 1;
+                wait(.01);
+                myLed3 = 0;
                 pbStatus = 0;
                 mySpeaker.PlayNote(400.0,0.1,0.8);
                 break;
+            case 0x04 :
+                //blink light for debugging
+                myLed4 = 1;
+                wait(.01);
+                myLed4 = 0;
+                pbStatus = 0;
+                mySpeaker.PlayNote(700.0,0.1,0.8);
+                break;  
         //no pb or multiple pbs hit
             default :
                 myLed2 = 0;
@@ -73,7 +126,10 @@ int main()
                 pbStatus = 0;
         } //end switch
         myLed1 = !myLed1; //blink to show loop is still running
-        wait(.5);
+        wait(1);
+        
     } //end while
 } //end main
+
+
 
