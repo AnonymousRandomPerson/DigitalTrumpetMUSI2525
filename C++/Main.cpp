ff5e5f6b9cdc5d1e933911bc29a19dab7dd03b20
AnonymousRandomPerson <chenghanngan.us@gmail.com>
2016-12-08 04:08:41 -0500

    Trumpet sound functioning on device

--- C++/Main.cpp
+++ C++/Main.cpp
@@ -1,7 +1,6 @@
 #include "mbed.h"
-#include "PinDetect.h"
+#include "pinDetect.h"
 #include "Speaker.h"
-
 
 // setup builtin LEDs
 DigitalOut myLed1(LED1);
@@ -40,6 +39,15 @@ Speaker mySpeaker(p21);
 volatile int pbStatus = 0;
 volatile int range = 0;
 
+AnalogOut DAC(p18);
+//global variables used by interrupt routine
+volatile int i=0;
+int counter=0;
+const int BUFFER_SIZE = 128;
+const int SAMPLE_RATE = 11025;
+float Analog_out_data[BUFFER_SIZE];
+bool needsUpdate = false;
+
 // Callback routine is interrupt activated by a debounced pb1 hit
 void pb1_hit_callback (void)
 {
@@ -59,8 +67,77 @@ void pb3_hit_callback (void)
     pbStatus = pbStatus | 0x04;
 
 }
+
+const float PERIOD = 440.0 / SAMPLE_RATE;
+const float TWO_PI = 6.28318530717959;
+const float MULTIPLIER = TWO_PI * PERIOD;
+const int SIN_PRECISION = 1024;
+float sinLookup[SIN_PRECISION];
+
+float sinApprox(float input)
+{
+    bool negative = false;
+    if (input < 0)
+    {
+        negative = true;
+        input = -input;
+    }
+    int roundInput = input * SIN_PRECISION / TWO_PI;
+    roundInput = roundInput & (SIN_PRECISION - 1);
+    float answer = sinLookup[roundInput];
+    if (negative)
+    {
+        answer = -answer;
+    }
+    return answer;
+}
+
+// Interrupt routine
+// used to output next analog sample whenever a timer interrupt occurs
+void Sample_timer_interrupt(void)
+{
+    // send next analog sample out to D to A
+    DAC = Analog_out_data[i];
+    // increment pointer and wrap around back to 0 at 128
+    i = (i+1) & (BUFFER_SIZE - 1);
+    if (i == 0)
+    {
+        needsUpdate = true;
+    }
+}
+
+void recalcBuffer(int offset)
+{
+    float curve = 0.8;
+    if (offset < 827)
+    {
+        curve = offset / 827.0 * 0.8 + 0.2;
+    }
+    else if (offset < 4134)
+    {
+        curve = 1.0 - (offset - 827.0) / 3307.0 * 0.2;
+    }
+    for(int k=0; k<BUFFER_SIZE; k++) {
+        float baseMultiplier = (k + offset) * MULTIPLIER;
+        Analog_out_data[k] = sinApprox(baseMultiplier);
+        Analog_out_data[k] += sinApprox(baseMultiplier * 2) * 0.7847;
+        Analog_out_data[k] += sinApprox(baseMultiplier * 3) * 0.8333;
+        Analog_out_data[k] += sinApprox(baseMultiplier * 4) * 0.7467;
+        Analog_out_data[k] += sinApprox(baseMultiplier * 5) * 0.4067;
+        Analog_out_data[k] += sinApprox(baseMultiplier * 6) * 0.4222;
+        Analog_out_data[k] += sinApprox(baseMultiplier * 7) * 0.3117;
+        Analog_out_data[k] += sinApprox(baseMultiplier * 8) * 0.2611;
+        Analog_out_data[k] += sinApprox(baseMultiplier * 9) * 0.205;
+        Analog_out_data[k] += sinApprox(baseMultiplier * 10) * 0.1375;
+        Analog_out_data[k] += sinApprox(baseMultiplier * 11) * 0.1006;
+        Analog_out_data[k] += sinApprox(baseMultiplier * 12) * 0.0658;
+        Analog_out_data[k] += sinApprox(baseMultiplier * 13) * 0.0428;
+        Analog_out_data[k] = (5.3181 + Analog_out_data[k] * curve) / 10.6362;
+    }
+}
+
 // main program
-void main()
+int main()
 {
 //setup four SPST push buttons
     pb1.mode(PullUp); //add internal pullup resistor
@@ -82,18 +159,31 @@ void main()
     bool valve1 = 0;
     bool valve2 = 0;
     bool valve3 = 0;
+    
+    // set up a timer to be used for sample rate interrupts
+    for(int k=0; k < SIN_PRECISION; k++) {
+        sinLookup[k] = sin(TWO_PI * k / float(SIN_PRECISION));
+    }
+    recalcBuffer(0);
+    Ticker Sample_Period;
+    Sample_Period.attach(&Sample_timer_interrupt, 1.0/(SAMPLE_RATE));
 
 //main loop 
     while(1) {
     //read and set vibration range
-            vreading = vibration.read();
-            pc.printf(" vibration = [%f]\n\r", vreading); 
+//            vreading = vibration.read();
+//            pc.printf(" vibration = [%f]\n\r", vreading); 
     //read and set pressure range
-            preading = pressure.read();
-            pc.printf(" pressure = [%f]\n\r\n", preading);
+//            preading = pressure.read();
+//            pc.printf(" pressure = [%f]\n\r\n", preading);
        //check pushbutton status for new input
        
-
+        if (needsUpdate)
+        {
+            needsUpdate = false;
+            counter += BUFFER_SIZE;
+            recalcBuffer(counter);
+        }
        
         switch (pbStatus) {
             case 0x01 :
@@ -125,11 +215,11 @@ void main()
                 myLed4 = 0;
                 pbStatus = 0;
         } //end switch
-        myLed1 = !myLed1; //blink to show loop is still running
-        wait(1);
+        //myLed1 = !myLed1; //blink to show loop is still running
+        //wait(1);
         
     } //end while
+    return 0;
 } //end main
-
 
 
--- C++/audioOut.cpp
+++ C++/audioOut.cpp
@@ -8,7 +8,64 @@
 AnalogOut DAC(p18);
 //global variables used by interrupt routine
 volatile int i=0;
-float Analog_out_data[128];
+int counter=0;
+const int BUFFER_SIZE = 128;
+const int SAMPLE_RATE = 11025;
+const float TWO_PI = 6.28318530717959;
+const float PERIOD = 1046.0 / SAMPLE_RATE;
+const float MULTIPLIER = TWO_PI * PERIOD;
+float Analog_out_data[BUFFER_SIZE];
+const int SIN_PRECISION = 1024;
+float sinLookup[SIN_PRECISION];
+bool needsUpdate = false;
+
+float sinApprox(float input)
+{
+    bool negative = false;
+    if (input < 0)
+    {
+        negative = true;
+        input = -input;
+    }
+    int roundInput = input * SIN_PRECISION / TWO_PI;
+    roundInput = roundInput & (SIN_PRECISION - 1);
+    float answer = sinLookup[roundInput];
+    if (negative)
+    {
+        answer = -answer;
+    }
+    return answer;
+}
+
+void recalcBuffer(int offset)
+{
+    float curve = 0.8;
+    if (offset < 827)
+    {
+        curve = offset / 827.0 * 0.8 + 0.2;
+    }
+    else if (offset < 4134)
+    {
+        curve = 1.0 - (offset - 827.0) / 3307.0 * 0.2;
+    }
+    for(int k=0; k<BUFFER_SIZE; k++) {
+        float baseMultiplier = (k + offset) * MULTIPLIER;
+        Analog_out_data[k] = sinApprox(baseMultiplier);
+        Analog_out_data[k] += sinApprox(baseMultiplier * 2) * 0.7847;
+        Analog_out_data[k] += sinApprox(baseMultiplier * 3) * 0.8333;
+        Analog_out_data[k] += sinApprox(baseMultiplier * 4) * 0.7467;
+        Analog_out_data[k] += sinApprox(baseMultiplier * 5) * 0.4067;
+        Analog_out_data[k] += sinApprox(baseMultiplier * 6) * 0.4222;
+        Analog_out_data[k] += sinApprox(baseMultiplier * 7) * 0.3117;
+        Analog_out_data[k] += sinApprox(baseMultiplier * 8) * 0.2611;
+        Analog_out_data[k] += sinApprox(baseMultiplier * 9) * 0.205;
+        Analog_out_data[k] += sinApprox(baseMultiplier * 10) * 0.1375;
+        Analog_out_data[k] += sinApprox(baseMultiplier * 11) * 0.1006;
+        Analog_out_data[k] += sinApprox(baseMultiplier * 12) * 0.0658;
+        Analog_out_data[k] += sinApprox(baseMultiplier * 13) * 0.0428;
+        Analog_out_data[k] = (5.3181 + Analog_out_data[k] * curve) / 10.6362;
+    }
+}
  
 // Interrupt routine
 // used to output next analog sample whenever a timer interrupt occurs
@@ -17,7 +74,11 @@ void Sample_timer_interrupt(void)
     // send next analog sample out to D to A
     DAC = Analog_out_data[i];
     // increment pointer and wrap around back to 0 at 128
-    i = (i+1) & 0x07F;
+    i = (i+1) & (BUFFER_SIZE - 1);
+    if (i == 0)
+    {
+        needsUpdate = true;
+    }
 }
  
 int main()
@@ -26,13 +87,23 @@ int main()
     Ticker Sample_Period;
     // precompute 128 sample points on one sine wave cycle 
     // used for continuous sine wave output later
-    for(int k=0; k<128; k++) {
-        Analog_out_data[k]=((1.0 + sin((float(k)/128*6.28318530717959)))/2.0);
-        // scale the sine wave from 0.0 to 1.0 - as needed for AnalogOut arg 
+    for(int k=0; k < SIN_PRECISION; k++) {
+        sinLookup[k] = sin(TWO_PI * k / float(SIN_PRECISION));
     }
+    recalcBuffer(0);
     // turn on timer interrupts to start sine wave output
     // sample rate is 500Hz with 128 samples per cycle on sine wave
-    Sample_Period.attach(&Sample_timer_interrupt, 1.0/(500));
+    Sample_Period.attach(&Sample_timer_interrupt, 1.0/(SAMPLE_RATE));
     // everything else needed is already being done by the interrupt routine
-    while(1) {}
+    trumpet = new TrumpetGenerator();
+    while(1)
+    {
+        if (needsUpdate)
+        {
+            needsUpdate = false;
+            counter += BUFFER_SIZE;
+            recalcBuffer(counter);
+        }
+    }
 }
+
