36cba20af4153fc692b499326e3c56ba160fc7f4
Schrum, Coulter S <cschrum3@gatech.edu>
2016-12-12 13:07:00 -0500

    Update Main.cpp

--- C++/Main.cpp
+++ C++/Main.cpp
@@ -1,79 +1,109 @@
 #include "mbed.h"
-#include "pinDetect.h"
-#include "Speaker.h"
+#include "PinDetect.h"
+
 
 // setup builtin LEDs
-DigitalOut myLed1(LED1);
-DigitalOut myLed2(LED2);
-DigitalOut myLed3(LED3);
-DigitalOut myLed4(LED4);
-
+  DigitalOut myLed1(LED1);
+  DigitalOut myLed2(LED2);
+  DigitalOut myLed3(LED3);
+  DigitalOut myLed4(LED4);
+  
 //Hardware Setup
 
 // setup vibration sensor
   AnalogIn vibration(p17);
   float vreading;
 //setup pressure sensor
-  AnalogIn pressure(p18);
+  AnalogIn pressure(p16);
   float preading;
   
   
-// Patterns for choosing which valve switch statement to use.
-  enum Pattern { pattern_a, pattern_b };
-// The current on/off status of the valves 123.
-  enum ValveStatus { v000, v100, v010, v110, v001, v101, v011, v111 };
+//Patterns for choosing which valve switch statement to use.
+  bool pattern_a = 1;
+  bool pattern_b = 0;
 // setup pc serial monitor
   Serial pc(USBTX, USBRX);
  
 
 // setup pushbuttons for debounced interrupt use
-PinDetect pb1(p23);
-PinDetect pb2(p24);
-PinDetect pb3(p25);
-
-// setup instance of new Speaker class, mySpeaker using pin 21
-// the pin must be a PWM output pin
-Speaker mySpeaker(p21);
+  PinDetect pb1(p23);
+  PinDetect pb2(p24);
+  PinDetect pb3(p25);
+  PinDetect pb4(p22);
 
 // pushbutton status - need volatile for safe interrupt R/W
-volatile int pbStatus = 0;
-volatile int range = 0;
+  volatile int pbStatus = 0;
+  volatile int range = 0;
 
-AnalogOut DAC(p18);
+  AnalogOut DAC(p18);
+  
 //global variables used by interrupt routine
-volatile int i=0;
-int counter=0;
-const int BUFFER_SIZE = 128;
-const int SAMPLE_RATE = 11025;
-float Analog_out_data[BUFFER_SIZE];
-bool needsUpdate = false;
+  volatile int i=0;
+  int counter=0;
+  const int BUFFER_SIZE = 256;
+  const int SAMPLE_RATE = 11025;
+  float Analog_out_data[BUFFER_SIZE];
+  bool needsUpdate = false;
+  const float PERIOD = 1.0 / SAMPLE_RATE;
+  const float TWO_PI = 6.28318530717959;
+  const float MULTIPLIER = TWO_PI * PERIOD;
+  const int SIN_PRECISION = 1024;
+  float sinLookup[SIN_PRECISION];
 
-// Callback routine is interrupt activated by a debounced pb1 hit
-void pb1_hit_callback (void)
-{
-    // CODE HERE WILL RUN WHEN INTERUPT IS GENERATED
-    pbStatus = pbStatus | 0x01;
-
-}
+// Callback routine for button on
+  void pb1_hit_callback_on (void)
+  {
+      // CODE HERE WILL RUN WHEN INTERUPT IS GENERATED
+      pbStatus = pbStatus | 0x01;
+  }
 // Callback routine is interrupt activated by a debounced pb2 hit
-void pb2_hit_callback (void)
-{
-    // CODE HERE WILL RUN WHEN INTERUPT IS GENERATED
-    pbStatus = pbStatus | 0x02;
-}
-void pb3_hit_callback (void)
-{
-    // CODE HERE WILL RUN WHEN INTERUPT IS GENERATED
-    pbStatus = pbStatus | 0x04;
+  void pb2_hit_callback_on (void)
+  {
+      // CODE HERE WILL RUN WHEN INTERUPT IS GENERATED
+      pbStatus = pbStatus | 0x02;
+  }
+  void pb3_hit_callback_on (void)
+  {
+      // CODE HERE WILL RUN WHEN INTERUPT IS GENERATED
+      pbStatus = pbStatus | 0x04;
+  }
+  
+  void pb4_hit_callback_on (void)
+  {
+      // CODE HERE WILL RUN WHEN INTERUPT IS GENERATED
+     pbStatus = pbStatus | 0x08;
+  }
 
-}
+// Callback routine for button off
+  void pb1_hit_callback_off (void)
+  {
+      // CODE HERE WILL RUN WHEN INTERUPT IS GENERATED
+      //0x06
+      pbStatus = pbStatus & 0x14;
 
-const float PERIOD = 1.0 / SAMPLE_RATE;
-const float TWO_PI = 6.28318530717959;
-const float MULTIPLIER = TWO_PI * PERIOD;
-const int SIN_PRECISION = 1024;
-float sinLookup[SIN_PRECISION];
+  }
+  //pb2
+  void pb2_hit_callback_off (void)
+  {
+      // CODE HERE WILL RUN WHEN INTERUPT IS GENERATED
+      //0x05
+      pbStatus = pbStatus & 0x13;
+  }
+  //pb3 
+  void pb3_hit_callback_off (void)
+  {
+      // CODE HERE WILL RUN WHEN INTERUPT IS GENERATED 0x03
+      pbStatus = pbStatus & 0x11;
+  }
+  //pb4
+  void pb4_hit_callback_off (void)
+  {
+      // CODE HERE WILL RUN WHEN INTERUPT IS GENERATED
+      pbStatus = pbStatus & 0x07;
+  }
 
+
+//member functions
 float sinApprox(float input)
 {
     bool negative = false;
@@ -155,22 +185,27 @@ int main()
     pb1.mode(PullUp); //add internal pullup resistor
     pb2.mode(PullUp);
     pb3.mode(PullUp);
+    pb4.mode(PullUp);
     // need a small delay for initial pullup to take effect due to capacitance
     wait(.01);
+    pb1.attach_deasserted(&pb1_hit_callback_on);
+    pb2.attach_deasserted(&pb2_hit_callback_on);
+    pb3.attach_deasserted(&pb3_hit_callback_on);
+    pb4.attach_deasserted(&pb4_hit_callback_on);
+    
+    
     // Setup Interrupt callback functions for each pb hit
-    pb1.attach_deasserted(&pb1_hit_callback);
-    pb2.attach_deasserted(&pb2_hit_callback);
-    pb3.attach_deasserted(&pb3_hit_callback);
+    pb1.attach_asserted(&pb1_hit_callback_off);
+    pb2.attach_asserted(&pb2_hit_callback_off);
+    pb3.attach_asserted(&pb3_hit_callback_off);
+    pb4.attach_asserted(&pb4_hit_callback_off);
     // Start sampling the pb inputs using interrupts
     pb1.setSampleFrequency();
     pb2.setSampleFrequency();
     pb3.setSampleFrequency();
+    pb4.setSampleFrequency();
 // pushbuttons are setup with pullups and running with a software debounce filter
 
-//boolean valves
-    bool valve1 = 0;
-    bool valve2 = 0;
-    bool valve3 = 0;
     
     // set up a timer to be used for sample rate interrupts
     for(int k=0; k < SIN_PRECISION; k++) {
@@ -182,57 +217,34 @@ int main()
     Sample_Period.attach(&Sample_timer_interrupt, 1.0/(SAMPLE_RATE));
 
 //main loop 
+    
     while(1) {
-    //read and set vibration range
-//            vreading = vibration.read();
-//            pc.printf(" vibration = [%f]\n\r", vreading); 
-    //read and set pressure range
-//            preading = pressure.read();
-//            pc.printf(" pressure = [%f]\n\r\n", preading);
-       //check pushbutton status for new input
-       
+        //code here will run when buffer is out of samples 
         if (needsUpdate)
         {
             needsUpdate = false;
             counter += BUFFER_SIZE;
-            recalcBuffer(counter, frequency, false);
-        }
-       
-        switch (pbStatus) {
-            case 0x01 :
-                myLed2 = 1;
-                wait(.01);
-                myLed2 = 0;
-                mySpeaker.PlayNote(200.0,0.1,0.8);
-                break;
-            case 0x02 :
-                //blink light for debugging
-                myLed3 = 1;
-                wait(.01);
-                myLed3 = 0;
-                pbStatus = 0;
-                mySpeaker.PlayNote(400.0,0.1,0.8);
-                break;
-            case 0x04 :
-                //blink light for debugging
-                myLed4 = 1;
-                wait(.01);
-                myLed4 = 0;
-                pbStatus = 0;
-                mySpeaker.PlayNote(700.0,0.1,0.8);
-                break;  
-        //no pb or multiple pbs hit
-            default :
-                myLed2 = 0;
-                myLed3 = 0;
-                myLed4 = 0;
-                pbStatus = 0;
-        } //end switch
-        //myLed1 = !myLed1; //blink to show loop is still running
-        //wait(1);
+            switch(pbStatus)
+            {
+            case 0x00: frequency = 261; break; // C
+            case 0x07: frequency = 277.2; break; // C#
+            case 0x05: frequency = 293.7; break; // D
+            case 0x06: frequency = 311.1; break; // Eb
+            case 0x03: frequency = 329.6; break; // E
+            case 0x01: frequency = 349.2; break; // F
+            case 0x02: frequency = 370.0; break; // F#
+            case 0x08: frequency = 392; break; // G
+            case 0x14: frequency = 415.3; break; // G#
+            case 0x11: frequency = 440; break; // A
+            case 0x09: frequency = 466.2; break; // A#
+            case 0x10: frequency = 493.9; break; // B
+            default: frequency = 261;
+            }
         
+            recalcBuffer(counter, frequency, false);
+            
+        }  
+    
     } //end while
-    return 0;
+    
 } //end main
-
-
